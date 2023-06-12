dbe2bd864877f5d3e544ddf1d36628c2cde61899
Cordoni_G <guidocordoni@yahoo.it>
2023-06-12 10:25:29 +0100

    Add files via upload

---
+++ apha_met_gc_mamba_installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create conda environment
+mamba create -n apha_meta python=3.6
+
+# Activate the environment
+mamba activate apha_meta
+#pip install
+pip install pyfiglet
+pip install biopython
+pip install pysam
+pip install pandas
+
+# Install required software and dependencies
+mamba install -c bioconda fastp multiqc trimmomatic megahit spades metabat2 pplacer checkm-genome kraken kraken2 krona prokka
+
+#sudo checkm data setRoot /home/guidocordoni/fsx/ranch-44/Other_projects/guido/db/checkm_db
+# Deactivate the environment
+mamba deactivate
+
+echo "Remember to activate apha-meta environment before to run the analysis (mamba activate mamba_meta)
+
